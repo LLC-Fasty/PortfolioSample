0989c01cd73c5b241f686ebe2c455119edf4d69a
Bharathi <163984487+LLC-Fasty@users.noreply.github.com>
2024-06-12 19:31:14 +0530

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -16,15 +16,10 @@ const AboutDetails = () => {
             Architect of Enchantment
           </h2>
           <p className="font-light  text-xs sm:text-sm md:text-base   ">
-            My journey in web development is powered by an array of mystical
-            tools and languages, with JavaScript casting the core of my
-            enchantments. I wield frameworks like React.js and Next.js with
-            precision, crafting seamless portals (websites) that connect realms
-            (users) across the digital universe. The ancient arts of the
-            Jamstack empower me to create fast, secure, and dynamic experiences,
-            while my design skills ensure every creation is not only functional
-            but visually captivating. Join me as I continue to explore new
-            spells and technologies to shape the future of the web.
+            Artificial Intelligence Engineer with 2+ years in Center of Excellence G.H.Raisoni College of Engineering, Nagpur.Also
+worked as a Web Developer Intern for 6 months in 10xGrowth. I am Open for any Software Development and AI/ML
+and Blockchain Related work. I am also a part of a Startup which is working on Blockchain Technology.
+
           </p>
         </ItemLayout>
 
